6578c5a7c8d82ec6638c3a7f4e31af272fd83e35
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-10-11 21:10:44 +0530

    First n numbers

---
+++ FirstNNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class FirstNNumbers{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        int i = 1;
+        while(i <= n){
+            System.out.println(i);
+            i++;
+        }       
+    }
+
+}
